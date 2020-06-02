b11c022eee3dd0bebb48bc1890ef769eb5abba0a
wilberC <wilbercarrascal@gmail.com>
2020-06-02 03:01:56 -0500

    callback-reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,23 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :reviewable, polymorphic: true
+
+  private
+
+  def ensure_count_is_initialized
+    return unless user.review_count.nil?
+
+    user.review_count = 0
+    user.save
+  end
+
+  def increase_review_count
+    user.review_count += 1
+    user.save
+  end
+
+  def decrease_review_count
+    user.review_count -= 1
+    user.save
+  end
 end
