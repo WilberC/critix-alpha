8ecc0837ab21fc660152ba510d6afcc4ff627307
wilberC <wilbercarrascal@gmail.com>
2020-06-02 22:50:02 -0500

    add_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class OlderThanSixteenValidator < ActiveModel::EachValidator
 end
 
 class User < ApplicationRecord
+  has_secure_password
   has_many :reviews
   validates :username, :email, presence: true, uniqueness: true
   validates :birth_date, presence: true, older_than_sixteen: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  # get 'users/new'
+  # get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :games, only: [:index, :show, :new, :create, :edit, :update]
   resources :companies, only: [:index, :show, :new, :create, :edit, :update]
+  # Routes for users
+  resources :users, only: [:new, :create]
+
+  # Routes for sessions
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  delete 'session', to: 'sessions#destroy'
 end
