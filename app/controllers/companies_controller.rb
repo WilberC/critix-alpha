03d820ba9eb488a4927ccf44775fdbf1e1759898
wilberC <wilbercarrascal@gmail.com>
2020-06-02 22:37:14 -0500

    finish_crud

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,4 +2,36 @@ class CompaniesController < ApplicationController
   def index
     @companies = Company.all
   end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to game_path(@company)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @company.update(company_params)
+      redirect_to(@company)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @company.destroy
+    redirect_to companies_path
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :description, :start_date, :country)
+  end
 end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,4 +8,23 @@ class GamesController < ApplicationController
     @platforms = @game.platforms.map(&:name).join(', ')
     @genres = @game.genres.map(&:name).join(', ')
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :summary, :release_date, :category, :rating, :parent_id)
+  end
 end
